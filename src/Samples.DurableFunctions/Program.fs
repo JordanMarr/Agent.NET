289fdcf7a0dd274e37040983b2a3cdad33a6ace4
Jordan Marr <JordanMarr@users.noreply.github.com>
2026-01-11 16:38:05 -0500

    Fixed az fn host builder to call ConfigureDurableExtension()

--- src/Samples.DurableFunctions/Program.fs
+++ src/Samples.DurableFunctions/Program.fs
@@ -1,12 +1,16 @@
 module Samples.DurableFunctions.Program
 
+open System
 open Microsoft.Extensions.Hosting
 open Microsoft.Azure.Functions.Worker
 
 [<EntryPoint>]
 let main args =
     HostBuilder()
-        .ConfigureFunctionsWorkerDefaults()
+        .ConfigureFunctionsWebApplication(Action<IFunctionsWorkerApplicationBuilder>(fun builder ->
+            // Explicitly configure the Durable Task extension
+            builder.ConfigureDurableExtension() |> ignore
+        ))
         .Build()
         .Run()
     0
